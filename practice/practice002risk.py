787ff7cc2e08b4155c06b14cb5af21b0d2721546
jieming2002 <jieming2002@gmail.com>
2017-11-23 19:58:05 +0800

    master

--- practice/practice002risk.py
+++ practice/practice002risk.py
@@ -3,32 +3,26 @@ __author__ = 'Skye 2017-11-23'
 from practice import practice001
 from practice import practice002
 
-def ProbEarly(pmf):
-    '''
-    提前出生
-    :param pmf:
-    :return:
-    '''
-    w < 38
-    pass
-
-def ProbOnTime(pmf):
-    '''
-    准时出生
-    :param pmf:
-    :return:
-    '''
-    38 <= w <= 40
-    pass
-
-def ProbLate(pmf):
-    '''
+def probCompute(table):
+    ''' 计算各个时间区间出生的婴儿所占的比例
     延后出生
     :param pmf:
     :return:
     '''
-    w > 40
-    pass
+    early = 0
+    onTime = 0
+    late = 0
+    for week in table.lengths:
+        if week > 40:
+            late += 1
+        elif week < 38:
+            early += 1
+        else:
+            onTime += 1
+
+    table.early = early / table.n
+    table.onTime = onTime / table.n
+    table.late = late / table.n
 
 def MakePmfs(data_dir):
     table, firsts, others = practice001.MakeTables(data_dir)
@@ -40,8 +34,20 @@ def MakePmfs(data_dir):
 
 def main(data_dir=''):
     pool, firsts, others =  MakePmfs(data_dir)
-    prob
-    pass
+    practice002.MakeTables(data_dir)
+    probCompute(pool)
+    probCompute(firsts)
+    probCompute(others)
+
+    # 计算延后出生和提前出生的相对风险
+    lateRiskPool = pool.late / pool.early
+    print('lateRiskPool = ', lateRiskPool)
+
+    lateRiskFirsts = firsts.late / firsts.early
+    print('lateRiskFirsts = ', lateRiskFirsts)
+
+    lateRiskOthers = others.late / others.early
+    print('lateRiskOthers = ', lateRiskOthers)
 
 if __name__ == '__main__':
     main('../data/')
